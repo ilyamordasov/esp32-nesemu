73fa7a44f908eecd9272c50808eea797d80e4494
Ilya M <carlogulliani@gmail.com>
2024-04-09 21:31:20 +0300

    modified:   esp32/main/main.c

--- esp32/main/main.c
+++ esp32/main/main.c
@@ -3,10 +3,10 @@
 #include "esp_system.h"
 #include "esp_event.h"
 #include "esp_log.h"
-#include "nvs_flash.h"
-#include "driver/gpio.h"
-#include "nofrendo.h"
-#include "esp_partition.h"
+// #include "nvs_flash.h"
+// #include "driver/gpio.h"
+// #include "nofrendo.h"
+// #include "esp_partition.h"
 
 #include <stdio.h>
 #include <inttypes.h>
@@ -16,21 +16,19 @@
 
 #define TAG "NES"
 
-
-
-char *osd_getromdata() {
-	char* romdata;
-	const esp_partition_t* part;
-	spi_flash_mmap_handle_t hrom;
-	esp_err_t err;
-	nvs_flash_init();
-	part=esp_partition_find_first(0x40, 1, NULL);
-	if (part==0) ESP_LOGE(TAG, "Couldn't find rom part!\n");
-	err=esp_partition_mmap(part, 0, 3*1024*1024, SPI_FLASH_MMAP_DATA, (const void**)&romdata, &hrom);
-	if (err!=ESP_OK) ESP_LOGE(TAG, "Couldn't map rom part!\n");
-	ESP_LOGI(TAG, "Initialized. ROM@%p\n", romdata);
-    return (char*)romdata;
-}
+// char *osd_getromdata() {
+// 	char* romdata;
+// 	const esp_partition_t* part;
+// 	spi_flash_mmap_handle_t hrom;
+// 	esp_err_t err;
+// 	nvs_flash_init();
+// 	part=esp_partition_find_first(0x40, 1, NULL);
+// 	if (part==0) ESP_LOGE(TAG, "Couldn't find rom part!\n");
+// 	err=esp_partition_mmap(part, 0, 3*1024*1024, SPI_FLASH_MMAP_DATA, (const void**)&romdata, &hrom);
+// 	if (err!=ESP_OK) ESP_LOGE(TAG, "Couldn't map rom part!\n");
+// 	ESP_LOGI(TAG, "Initialized. ROM@%p\n", romdata);
+//     return (char*)romdata;
+// }
 
 
 esp_err_t event_handler(void *ctx, system_event_t *event)
